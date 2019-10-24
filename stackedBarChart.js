b4a327eb226035010049a4d09831a5e14fd934ca
Brendan O'Handley <brendan.ohandley@gmail.com>
2019-10-23 22:30:27 -0400

    organize data, build start of stacked bar chart

--- stackedBarChart.js
+++ stackedBarChart.js
@@ -1,64 +1,152 @@
-var padding = 10;
-var barHeight = 20;
-var fontSize = 20;
-var formatPercent = d3.format(".0%");
+d3.csv("agedistribution.csv", function(error, ageDist){
+	d3.csv("worldpopulation.csv", function(error, worldPop){
+		console.log(worldPop)
+		
+		console.log(ageDist)
+		ageDist.sort(function(a,b){ return a.age - b.age})
+		worldPop.forEach(function(w){
+			ageDist.forEach(function(a){
+				if(w.year == a.year)
+					w[a.age] = w["population (in millions)"] * a.value
+			});
+		});
 
-var svg = d3.select("svg");
+		var data = worldPop;
+		data.sort(function(a,b){return b.year - a.year;})
+		// console.log(data);
 
-var ages = d3.csv("ageDistribution.csv")
+		var svg = d3.select("svg"),
+		    margin = {top: 20, right: 20, bottom: 30, left: 40},
+		    width = +svg.attr("width") - margin.left - margin.right,
+		    height = +svg.attr("height") - margin.top - margin.bottom,
+		    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("agedistribution.csv", displayAgeDist());
+	      var tooltip = svg.append("g")
+			    .attr("class", "tooltip")
+			    .style("display", "none");
+			      
+			  tooltip.append("rect")
+			    .attr("width", 60)
+			    .attr("height", 20)
+			    .attr("fill", "white")
+			    .style("opacity", 1);
 
-// d3.csv("world-population.csv", displayWorldPop());
+			  tooltip.append("text")
+			    .attr("x", 30)
+			    .attr("dy", "1.2em")
+			    .style("text-anchor", "middle")
+			    .attr("font-size", "12px")
+			    .attr("font-weight", "bold");
 
-function displayAgeDist(){
-	// create a scale for the x axis at the top of the graph to %14
-	var xScale = d3.scaleLinear()
-		.domain([0, .14])
-		.range([0,500]);
+		    // set x scale
+				var x = d3.scaleBand()
+				    .rangeRound([0, width-100])
+				    .paddingInner(0.05)
+				    .align(0.1);
 
-	// create the x axis with the above scale
-	var xAxis = d3.axisBottom(xScale)
-		.ticks(8)
-		.tickFormat(formatPercent);
+				var y = d3.scaleLinear()
+				    .rangeRound([height, 0]);
 
-	// append the x axis
-	d3.select('svg')
-		.append('g')
-		.attr("transform", "translate(100, 520)")
-		.call(xAxis);
+		// The colors of apricots, blueberries, and cherries
+				var colors = [
+					'#159ED0',	'#43C201',	'#45FA96',
+					'#97B1BF',	'#3CAAD7',	'#10DDC1',
+					'#F14B35',	'#C8BB34',	'#2DBEE6',	
+					'#D138C0',	'#4BF704',	'#EAE352',	
+					'#82B4EE',	'#A670A3',	'#ADBE58',	
+					'#E55F21',	'#D1BC41'
+				];
+					
 
-	// create a scale for the x axis at the top of the graph to %14
-	var yScale = d3.scaleLinear()
-		.domain([.14, 0])
-		.range([0,500]);
+				// set the colors
+				var z = d3.scaleOrdinal()
+				    .range(colors);
 
-	// create the Y axis with the above scale
-	var yAxis = d3.axisLeft(yScale)
-		.ticks(8)
-		.tickFormat(formatPercent);
+				var keys = [...new Set(ageDist.map(function(d){ return d.age; }))];
+				// console.log(keys);
 
-	// append the x axis
-	d3.select('svg')
-		.append('g')
-		.attr("transform", "translate(100, 20)")
-		.call(yAxis);
-}
+				var stackGenerator = d3.stack()
+				  .keys(keys);
 
-function displayWorldPop(){
-		// create a scale for the x axis at the top of the graph to %14
-	var xScale = d3.scaleLinear()
-		.domain([0, .14])
-		.range([0,560]);
+				var yMax = d3.max(data, function(d) {  
+					var total = 0;
+					keys.forEach(function(a){
+						total += d[a];
+					});
 
-	// create the x axis with the above scale
-	var xAxis = d3.axisTop(xScale)
-		.ticks(8)
-		.tickFormat(formatPercent);
+					return total;
+				});
 
-	// append the x axis
-	d3.select('svg')
-		.append('g')
-		.attr("transform", "translate(100, 20)")
-		.call(xAxis);
-}
+				x.domain(data.map(function(d) { return d.year; }));
+			  y.domain([0, yMax]).nice();
+			  z.domain(keys);
+
+
+			  g.append("g")
+			    .selectAll("g")
+			    .data(d3.stack().keys(keys)(data))
+			    .enter().append("g")
+			      .attr("fill", function(d) { return z(d.key); })
+			    .selectAll("rect")
+			    .data(function(d) { return d; })
+			    .enter().append("rect")
+			      .attr("x", function(d) { return x(d.data.year); })
+			      .attr("y", function(d) { return y(d[1]); })
+			      .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+			      .attr("width", x.bandwidth())
+			    .on("mouseover", function() { tooltip.style("display", null); })
+			    .on("mouseout", function() { tooltip.style("display", "none"); })
+			    .on("mousemove", function(d) {
+			      // console.log(d);
+			      var xPosition = d3.mouse(this)[0] - 5;
+			      var yPosition = d3.mouse(this)[1] - 5;
+			      tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+			      tooltip.select("text").text((d[1]-d[0]).toFixed(2)+"m");
+			    });
+
+			  g.append("g")
+			      .attr("class", "axis")
+			      .attr("transform", "translate(0," + height + ")")
+			      .call(d3.axisBottom(x));
+
+			  g.append("g")
+			      .attr("class", "axis")
+			      .call(d3.axisLeft(y).ticks(null, "s"))
+			    .append("text")
+			      .attr("x", 2)
+			      .attr("y", y(y.ticks().pop()) + 0.5)
+			      .attr("dy", "0.32em")
+			      .attr("fill", "#000")
+			      .attr("font-weight", "bold")
+			      .attr("text-anchor", "start");
+
+			  var legend = g.append("g")
+			      .attr("font-family", "sans-serif")
+			      .attr("font-size", 10)
+			      .attr("text-anchor", "end")
+			    .selectAll("g")
+			    .data(keys.slice().reverse())
+			    .enter().append("g")
+			      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+			  legend.append("rect")
+			      .attr("x", width - 19)
+			      .attr("width", 19)
+			      .attr("height", 19)
+			      .attr("fill", z)
+			      // .attr("transform", "translate(30,0)")
+
+			  legend.append("text")
+			      .attr("x", width - 24)
+			      .attr("y", 9.5)
+			      .attr("dy", "0.32em")
+			      .text(function(d) { return d; })
+			      // .attr("transform", "translate(30,0)");
+
+
+
+	});
+});
+
+
+				
