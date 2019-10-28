1ebe8bf1648f2b5e3fae69aa805f1bd8bb9e16e5
Brendan O'Handley <brendan.ohandley@gmail.com>
2019-10-27 23:03:58 -0400

    refactor and clean code. add a title and move radio buttons to top

--- stackedBarChart.js
+++ stackedBarChart.js
@@ -1,3 +1,4 @@
+// TASK 1: Read the data from both files
 d3.csv("agedistribution.csv", function(error, ageDist){
 	d3.csv("worldpopulation.csv", function(error, worldPop){
 		var svg = d3.select("svg");
@@ -5,7 +6,7 @@ d3.csv("agedistribution.csv", function(error, ageDist){
 		var worldByCount = JSON.parse(JSON.stringify(worldPop));
 		var worldByPortion = JSON.parse(JSON.stringify(worldPop));
 		
-		// compute the count
+		// TASK 1: calculate actual population counts 
 		worldByCount.forEach(function(w){
 			ageDist.forEach(function(a){
 				if(w.year == a.year)
@@ -13,7 +14,7 @@ d3.csv("agedistribution.csv", function(error, ageDist){
 			});
 		});
 
-		// build data for portion
+		// TASK 2: get data for displaying portion
 		worldByPortion.forEach(function(w){
 			ageDist.forEach(function(a){
 				if(w.year == a.year)
@@ -21,19 +22,17 @@ d3.csv("agedistribution.csv", function(error, ageDist){
 			});
 		});
 
-		// assign the computed
-		var data = worldByCount;
+		// TASK 2: display by portion as default
+		displayPopulation(svg, worldByPortion, ageDist, 0);
 
-		displayPopulation(svg, worldByCount, ageDist, 1);
-
-		 		// Creating radio buttons
+		// TASK 4: Creating radio buttons
 		var shapeData = ["by portion", "by count"], 
-    		j = 1;  // Choose the by portion as default
+    		j = 0;// Choose the by portion as default
 
-		// Create the shape selectors
+		// TASK 4: Create the shape selectors or radio buttons
 		var form = d3.select("form")
 
-		// build radio buttons
+		// TASK 4: build radio buttons
 		var labels = form.selectAll("label")
 		    .data(shapeData)
 		    .enter()
@@ -46,45 +45,46 @@ d3.csv("agedistribution.csv", function(error, ageDist){
 		    .attr("value",function(d, i) {return d;})
 		    .property("checked", function(d, i) {return i===j;})
 		
+		// TASK 4: switch between two stacked barcharts
 		var inputs = form.selectAll("input")
 		    .on("click", function(d, i){
 		    	var choice = d3.select(this).attr("value");
 
 		    	if(choice == "by count"){
-		    		svg.html('')
+		    		// TASK 3: show age distribution by count
+		    		svg.html('');
 		    		displayPopulation(svg, worldByCount, ageDist, 1);
 		    	} else if (choice == "by portion"){
-		    		svg.html('')
+
+		    		svg.html('');
 		    		displayPopulation(svg, worldByPortion, ageDist, 0);
 		    	}
 		    })
-
 
 	});
 });
 
 function displayPopulation(svg, data, ageDist, v=0){
 
-	// access the svg element, set margin, width, height, and append a group
-	
+	// access the svg element, set margin, width, height, and append a group	
 	var	margin = {top: 20, right: 20, bottom: 30, left: 100},
 		width = +svg.attr("width") - margin.left - margin.right,
 		height = +svg.attr("height") - margin.top - margin.bottom,
 		g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	// start to build the tooltips	
+	// TASK 2: start to build the tooltips	
 	var tooltip = svg.append("g")
 	    .attr("class", "tooltip")
 	    .style("display", "none");
 	    
-	// build rect display for the tool tip  
+	// TASK 2: build rect display for the tool tip  
 	tooltip.append("rect")
 		.attr("width", 60)
 		.attr("height", 20)
 		.attr("fill", "white")
 		.style("opacity", 1);
 
-	// configure the text for the tooltip
+	// TASK 2: configure the text for the tooltip
 	tooltip.append("text")
 		.attr("x", 30)
 		.attr("dy", "1.2em")
@@ -122,7 +122,7 @@ function displayPopulation(svg, data, ageDist, v=0){
 	
 	// build the stack generator
 	var stackGenerator = d3.stack()
-	  .keys(keys);
+	  .keys(keys.reverse());
 
 	// find the largest value for count
 	var yMax = d3.max(data, function(d) {  
@@ -135,37 +135,37 @@ function displayPopulation(svg, data, ageDist, v=0){
 		return total;
 	});
 
-	console.log(yMax)
-
 	// create the domains
 	x.domain(data.map(function(d) { return d.year; }));
 	y.domain([0, yMax]);
 	z.domain(keys);
 
-	if(v){
-	// build each column of the graph by stacks and add the tooltips
-	var graph = g.append("g")
-	    .selectAll("g")
-	    .data(d3.stack().keys(keys)(data))
-	    .enter().append("g")
-	      	.attr("fill", function(d) { return z(d.key); })
-	    .selectAll("rect")
-	    .data(function(d) { return d; })
-	    .enter().append("rect")
-	      	.attr("x", function(d) { return x(d.data.year); })
-	      	.attr("y", function(d) { return y(d[1]); })
-	      	.attr("width", x.bandwidth())
-	    .on("mouseover", function() { tooltip.style("display", null); })
-	    .on("mouseout", function() { tooltip.style("display", "none"); })
-	    .on("mousemove", function(d) {
-	      	var xPosition = d3.mouse(this)[0] - 5;
-	      	var yPosition = d3.mouse(this)[1] - 5;
-	      	tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-	      	tooltip.select("text").text((d[1]-d[0]).toFixed(2)+"m");
-	    })
-	} else {
-		console.log(data)
-			// build each column of the graph by stacks and add the tooltips
+	if(v){ // TASK 3: build the display for actual count
+		// format the y axis to go to 10 billion
+		y.nice();
+		
+		// build each column of the graph by stacks and add the tooltips
+		var graph = g.append("g")
+		    .selectAll("g")
+		    .data(d3.stack().keys(keys)(data))
+		    .enter().append("g")
+		      	.attr("fill", function(d) { return z(d.key); })
+		    .selectAll("rect")
+		    .data(function(d) { return d; })
+		    .enter().append("rect")
+		      	.attr("x", function(d) { return x(d.data.year); })
+		      	.attr("y", function(d) { return y(d[1]); })
+		      	.attr("width", x.bandwidth())
+		    .on("mouseover", function() { tooltip.style("display", null); })
+		    .on("mouseout", function() { tooltip.style("display", "none"); })
+		    .on("mousemove", function(d) {
+		      	var xPosition = d3.mouse(this)[0] - 5;
+		      	var yPosition = d3.mouse(this)[1] - 5;
+		      	tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+		      	tooltip.select("text").text((d[1]-d[0]).toFixed(2)+"m");
+		    })
+	} else { // TASK 2: build the display for portion
+		// build each column of the graph by stacks and add the tooltips
 		var graph = g.append("g")
 		    .selectAll("g")
 		    .data(d3.stack().keys(keys)(data))
@@ -187,17 +187,18 @@ function displayPopulation(svg, data, ageDist, v=0){
 	      	})
 	}
 
-
-
 	smoothTransition(graph, y);
 
+	// TASKS 2 and 3: add axes for both displays
 	addAxes(g, x, y, height, v);
 
+	// TASK 5: build legend for mapping age groups to colors
 	buildLegend(g, z, keys, width);
 
 }
 
 function buildLegend(g, z, keys, width) {
+	// TASK 5: build legend for mapping age groups to colors
 	var legend = g.append("g")
 	    .attr("font-family", "sans-serif")
 	    .attr("font-size", 10)
@@ -234,20 +235,23 @@ function smoothTransition(graph, y) {
 
 				
 function addAxes(g, x, y, height, v=0) {
-		// add the x axis
+	// TASKS 2 and 3: add axes for both displays
+	// add the x axis
 	g.append("g")
 		.attr("class", "axis")
 		.attr("transform", "translate(0," + height + ")")
 		.call(d3.axisBottom(x));
 
-	if(v){
+	if(v){ // TASKS 3: add axes for count display
 		// append the y axis
 		g.append("g")
 			.attr("class", "axis")
 			.call(d3.axisLeft(y).ticks(null, "s").tickFormat(function(d){ 
 				if (d == 0){
 					return d;
-				} else {
+				} else if(d > 9000){
+					return 10 + ',000,000,000'
+				}else {
 					d = d +'';
 					return d[0] + ',000,000,000';
 				}
@@ -260,8 +264,8 @@ function addAxes(g, x, y, height, v=0) {
 			.attr("fill", "#000")
 			.attr("font-weight", "bold")
 			.attr("text-anchor", "start")
-			// .remove();
-	} else {
+			
+	} else { // TASKS 2: add axes for portion display
 		g.append("g")
 			.attr("class", "axis")
 			.call(d3.axisLeft(y).ticks(null, "s").tickFormat(function(d){ return d + '%'}))
@@ -272,7 +276,7 @@ function addAxes(g, x, y, height, v=0) {
 			.attr("fill", "#000")
 			.attr("font-weight", "bold")
 			.attr("text-anchor", "start")
-			// .remove();
+			
 	}
 }
 
